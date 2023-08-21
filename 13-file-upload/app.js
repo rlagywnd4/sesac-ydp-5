dd6cfb4da68fdd881ad246556062ed8602d97120
rlagywnd4 <rlagywnd4@gmail.com>
2023-08-21 15:32:18 +0900

    study: multer 세부 설정

--- 13-file-upload/app.js
+++ 13-file-upload/app.js
@@ -4,8 +4,31 @@ const PORT = 8000;
 
 // multer 관련 설정
 const multer = require('multer');
+const path = require('path'); //경로에 관한 내장 모듈
 const upload = multer({
   dest: 'uploads/', // dest: 클라이언트가 업로드한 파일을 저장할 서버측 경로
+});
+// multer 세부 설정
+const uploadDetail = multer({
+  // storage: 저장할 공간에 대한 정보
+  storage: multer.diskStorage({
+    // done: callback(콜백함수)
+    // done(null, xx) 여기서  null은 error를 의미하는 매개변수
+    // 에러가 없으므로 "null"이라고 전달하여 콜백 함수를 호출!
+    // done안의 첫번째 인수에는 에러가 있다면 에러를, 두번째 인수에는 실제 경로나, 파일이름을 넣는다.
+    destination(req, file, done) {
+      done(null, 'uploads/'); // 파일을 업로드할 경로 설정
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname); // 파일 "확장자"를 추출
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext); // basename: 원본 파일에서 확장자를 제외한 파일 이름만 추출
+      // 파일명 결과에 왜 Date.now를 추가해서 저장할까?
+      // 1. 파일 이름 중복을 막기 위해
+      // 2. 파일 이름만 보고 파일이 저장된 시점 유추 가능
+    },
+  }),
+  // limits: 파일 제한 정보
+  limits: { fileSize: 5 * 1024 * 1024 }, //5MB
 });
 
 app.set('view engine', 'ejs');
@@ -26,7 +49,8 @@ app.get('/', (req, res) => {
 // multer 설정(upload 변수)에 따라 파일을 업로드 한 후, req.file 객체 생성
 // single() 인자는 input태그의 name 속성과 일치시켜야 함
 // app.post('/upload', upload.single('form태그의 input태그(type='file')의 name'), (req, res) => {
-app.post('/upload', upload.single('userfile'), (req, res) => {
+// app.post('/upload', upload.single('userfile'), (req, res) => {
+app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
   // req.file: 파일 업로드 정보
   // req.body: 파일 외의 정보들
   console.log(req.file);
