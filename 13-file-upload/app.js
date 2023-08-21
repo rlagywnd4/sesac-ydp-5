54147faeb739d77bf78d6df545bad8c93862f77f
rlagywnd4 <rlagywnd4@gmail.com>
2023-08-21 16:30:33 +0900

    study: multer array, fields

--- 13-file-upload/app.js
+++ 13-file-upload/app.js
@@ -69,3 +69,25 @@ app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
 //     path: 'uploads\\157d0a48203e59605bdc66b58f817e6a', // 업로드된 파일 전체 경로
 //     size: 1087 // 파일 크기
 //   }
+
+// 2. array(): 여러 파일을 한 번에 업로드
+// uploadDetail.array('userfiles'): 클라이언트 요청이 들어오면
+// multer 설정(uploadDetail 변수)에 따라 파일을 업로드한 후, req.files 객체 생성
+app.post('/upload/array', uploadDetail.array('userfiles'), (req, res) => {
+  console.log(req.files); // [{파일1_정보}, {파일2_정보}, ...] : 배열 형태로 각 파일 정보를 출력
+  console.log(req.body);
+  res.send('하나의 인풋에 여러 파일 업로드 완료!');
+});
+
+// 3. fields(): 여러 파일을 각각 인풋에 업로드
+// req.files에서 팡리 정보를 확인
+// fields() 매개 변수로 input 태그의 name을 각각 넣기
+app.post(
+  '/upload/fields',
+  uploadDetail.fields([{ name: 'userfile1' }, { name: 'userfile2' }]),
+  (req, res) => {
+    console.log(req.files); // { userfile1: [{파일_정보}], userfile2: [ {파일_정보}]} 객체 안에 배열 형태로 각 파일 저보
+    console.log(req.body);
+    res.send('하나의 인풋에 여러 파일 업로드 완료!');
+  }
+);
