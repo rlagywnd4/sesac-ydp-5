260eea7e5085fb3ff6dfe69614cd9273ee92b509
rlagywnd4 <rlagywnd4@gmail.com>
2023-10-06 15:35:42 +0900

    study: react useEffect 이해하기(라이플사이클)

--- 27-ref-lifecycle/src/App.js
+++ 27-ref-lifecycle/src/App.js
@@ -1,4 +1,6 @@
 import Ex1 from './Ex1';
+import LifeCycleClass from './LifeCycleClass';
+import LifeCycleFunc from './LifeCycleFunc';
 import RefSample1 from './RefSample1';
 import RefSample2 from './RefSample2';
 import RefSample3 from './RefSample3';
@@ -8,20 +10,25 @@ function App() {
   return (
     <div className="App">
       {/* 함수형 컴포넌트; useRef()로 DOM요소에 직접 접근 */}
-      <RefSample1 />
-      <hr />
+      {/* <RefSample1 />
+      <hr /> */}
 
       {/* 함수형 컴포넌트; useRef() 로 로컬 변수 사용 */}
-      <RefSample2 />
-      <hr />
+      {/* <RefSample2 />
+      <hr /> */}
 
-      <RefSample3 />
-      <hr />
+      {/* <RefSample3 />
+      <hr /> */}
 
       {/* 클래스형 컴포넌트 */}
-      <RefSample4 />
+      {/* <RefSample4 />
       <hr />
+
       <Ex1 />
+      <hr /> */}
+
+      {/* <LifeCycleFunc /> */}
+      <LifeCycleClass />
     </div>
   );
 }
