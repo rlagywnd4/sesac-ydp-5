5c98808b3424a78176ce128d36c6fda3b4df99e3
rlagywnd4 <rlagywnd4@gmail.com>
2023-10-18 18:46:23 +0900

    delete: useless imports

--- 31-redux/src/prac/Bank.js
+++ 31-redux/src/prac/Bank.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
-import { depositMoney } from '../store/acountReducer';
-import { useDispatch } from 'react-redux';
 import { AcountsContainer } from '../containers/AcountsContainer';
 
 export default function Bank() {
